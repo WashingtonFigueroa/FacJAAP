c6e2698db29a86b8d97068a497c484e6958e3078
DTE <w.figo.1991@gmail.com>
2018-08-17 09:48:53 -0500

    Actualizacion bdd

--- BDD/antiguajaap.sql
+++ BDD/antiguajaap.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 17, 2018 at 04:48 PM
+-- Generation Time: Aug 17, 2018 at 04:43 PM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.1.15
 
